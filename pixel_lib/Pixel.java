fbad559a87c816ffbf44a6a54edc5ebf4528a48a
Jakob <36604022+bakobjirner@users.noreply.github.com>
2021-12-11 17:19:08 +0100

    added PixelLib and testProgram

---
+++ pixel_lib/Pixel.java
@@ -0,0 +1,12 @@
+public class Pixel {
+
+    public Pixel(int r, int g, int b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public int r;
+    public int g;
+    public int b;
+}

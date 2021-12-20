4ef7ec34878c873bfaf77b62063f3c6662060bd0
Leonhard Bosch <lbosch@stud.hs-heilbronn.de>
2021-12-20 14:39:04 +0100

    Added taskScheduler to ESP32

--- esp_mesh/src/main.cpp
+++ esp_mesh/src/main.cpp
@@ -16,37 +16,31 @@ using namespace std;
 // LEDs
 #define   NUM_LEDS        6
 #define   DATA_PIN        2
-#define   BRIGTHNESS      128
+#define   BRIGTHNESS      128 // On startup
 
 Scheduler userScheduler; // to control your personal task
 painlessMesh mesh;
 CRGB leds[NUM_LEDS];
+String lastMessage = "";
 
-// User stub
-void sendMessage() ; // Prototype so PlatformIO doesn't complain
+void setLEDs() {
+  if(lastMessage.isEmpty()){
+    return;
+  }
 
-// Needed for painless library
-void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-
-  const char* toDecode = msg.c_str();
+  const char* toDecode = lastMessage.c_str();
 
   size_t outputLength;
   unsigned char* decoded = base64_decode((const unsigned char*) toDecode, strlen(toDecode), &outputLength);
- 
-  Serial.print("Length of decoded message: ");
-  Serial.println(outputLength);
- 
-  Serial.printf("%.*s", outputLength, decoded);
 
   if(outputLength == 1){
     FastLED.setBrightness(decoded[0]);
-    mesh.sendSingle(from, "Set brightness to " + (uint8_t) decoded[0]);
+    //mesh.sendSingle(from, "Set brightness to " + (uint8_t) decoded[0]);
     return;
   }
 
   if(outputLength != NUM_LEDS*3){
-    mesh.sendSingle(from, "Invalid message length, expected 6, got " + outputLength);
+    //mesh.sendSingle(from, "Invalid message length, expected 18, got " + outputLength);
     return;
   }
 
@@ -59,12 +53,23 @@ void receivedCallback( uint32_t from, String &msg ) {
   FastLED.show();
 }
 
+Task taskSetLEDs(TASK_SECOND * 1, TASK_FOREVER, &setLEDs);
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  // TODO here output
+  lastMessage = msg;
+}
+
 void newConnectionCallback(uint32_t nodeId) {
     Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
 
 void changedConnectionCallback() {
-  Serial.printf("Changed connections\n");
+  //Serial.printf("Changed connections\n");
 }
 
 void nodeTimeAdjustedCallback(int32_t offset) {
@@ -73,6 +78,20 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 
 void setup() {
   Serial.begin(115200);
+
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGTHNESS);
+
+  // Show turn on animation
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i] = CRGB::Fuchsia;
+  }
+  FastLED.show();
+  delay(1000);
+  for(int i = 0; i < NUM_LEDS; i++){
+    leds[i] = CRGB::Black;
+  }
+  FastLED.show();
 
   //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
@@ -83,8 +102,9 @@ void setup() {
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-  FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGTHNESS);
+  userScheduler.addTask(taskSetLEDs);
+  taskSetLEDs.enable();
+
 }
 
 void loop() {
